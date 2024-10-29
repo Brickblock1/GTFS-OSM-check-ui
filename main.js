2c4c4fe686541cd7a78dadec67706bda0d2e1dd8
Brickblock1 <100433052+Brickblock1@users.noreply.github.com>
2024-10-29 15:18:18 +0100

    Add support for start and end date

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function handle_stop(member, thing) {
-  if (member.role.includes("platform")) {
+function openstreetmaps(type, id) {
+  window.open("https://openstreetmaps.org/" + type + "/" + id, "_blank");
+}
+
+function handle_stop(stop, layer, stop_order, role) {
+  if (role.includes("platform")) {
     colour = "green";
   } else {
     colour = "red";
@@ -7,82 +11,70 @@ function handle_stop(member, thing) {
 
   // Place a marker if node
 
-  if (member.type == "node") {
-    if (member.role.includes("platform")) {
-      var stop = L.marker([member.lat, member.lon], {
-        icon: greenDot,
-      }).addTo(map);
-    } else {
-      var stop = L.marker([member.lat, member.lon], {
-        icon: redDot,
-      }).addTo(map);
-    }
+  if (stop.type == "node") {
+    var map_stop = L.circle([stop.lat, stop.lon], {
+      color: colour,
+      fillColor: "grey",
+      fillOpacity: 1,
+      weight: 4,
+      radius: 1,
+    }).addTo(layer);
 
     // Place line if way // Add better support for areas
-  } else if (member.type == "way") {
+  } else if (stop.type == "way") {
     let latlngs = [];
-    for (let p of member.geometry) {
+    for (let p of stop.geometry) {
       latlngs.push([p.lat, p.lon]);
     }
-    for (let global_entry of thing.elements) {
-      if (global_entry.id == member.ref) {
-        if (global_entry.tags.area == "yes") {
-          var stop = L.polygon(latlngs, {
-            color: colour,
-          }).addTo(map);
-        } else {
-          var stop = L.polyline(latlngs, {
-            color: colour,
-          }).addTo(map);
-        }
-      }
+    if (stop.tags.area == "yes") {
+      var map_stop = L.polygon(latlngs, {
+        color: colour,
+      }).addTo(layer);
+    } else {
+      var map_stop = L.polyline(latlngs, {
+        color: colour,
+      }).addTo(layer);
     }
+  }
 
-    // Place polygon if multipolygon after ways have been merged into an array of points
-  } else if (member.type == "relation") {
-    for (let global_entry of thing.elements) {
-      if (global_entry.id == member.ref) {
-        let latlngs = [];
-        let un_latlngs = [];
-        for (let global_multipolygon_way of global_entry.members) {
-          for (let i in global_multipolygon_way.geometry) {
-            p = global_multipolygon_way.geometry[i];
-            p2 = global_multipolygon_way.geometry.at(length - i);
-            if (
-              [
-                global_multipolygon_way.geometry[i].lat,
-                global_multipolygon_way.geometry[i].lon,
-              ] == latlngs.at(-1) ||
-              latlngs.length == 0
-            ) {
-              latlngs.push([p.lat, p.lon]);
-            } else {
-              latlngs.push([p.lat, p.lon]);
-            }
-          }
+  // Place polygon if multipolygon after ways have been merged into an array of points
+  else if (stop.type == "relation") {
+    let latlngs = [];
+    for (let global_multipolygon_way of stop.members) {
+      for (let i in global_multipolygon_way.geometry) {
+        p = global_multipolygon_way.geometry[i];
+        p2 = global_multipolygon_way.geometry.at(length - i);
+        if (
+          [
+            global_multipolygon_way.geometry[i].lat,
+            global_multipolygon_way.geometry[i].lon,
+          ] == latlngs.at(-1) ||
+          latlngs.length == 0
+        ) {
+          latlngs.push([p.lat, p.lon]);
+        } else {
+          latlngs.push([p.lat, p.lon]);
         }
-        var stop = L.polygon(latlngs, { color: colour }).addTo(map);
       }
     }
+    var map_stop = L.polygon(latlngs, { color: colour }).addTo(layer);
   }
 
   //Make popup for platform
 
-  for (let global_entry of thing.elements) {
-    if (global_entry.id == member.ref) {
-      if (global_entry.tags.name == null) {
-        stop_name = "Name not in OSM";
-      } else {
-        stop_name = global_entry.tags.name
-      }
-      stop.bindTooltip(`${stop_name} (${global_entry.tags["gtfs:platform_code:SE-003-UL"]})`);
-      popup_text = ""
-      for (entry of Object.entries(global_entry.tags)) {
-        if (!(entry[0] == "stop_lon" || entry[0] == "stop_lat")) {
-          popup_text = popup_text + entry[0] + "=" + entry[1] + "<br>"
-        }
-      }
-      stop.bindPopup(popup_text)
+  if (stop.tags != null) {
+    if (stop.tags.name == null) {
+      stop_name = "Name not in OSM";
+    } else {
+      stop_name = stop.tags.name;
     }
+    map_stop.bindTooltip(`${stop_name} (${stop_order})`);
+    popup_text = `<input type="button" value="Open at openstreetmaps.org" onclick="openstreetmaps('${stop.type}', '${stop.id}')"><br>`;
+    for (entry of Object.entries(stop.tags)) {
+      if (!(entry[0] == "stop_lon" || entry[0] == "stop_lat")) {
+        popup_text = popup_text + entry[0] + "=" + entry[1] + "<br>";
+      }
+    }
+    map_stop.bindPopup(popup_text);
   }
 }
